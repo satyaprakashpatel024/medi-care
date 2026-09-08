554e9247f1a053860dd0afcbfb6b73a2f26f896b
satyaprakashpatel024 <1008tonystark@gmail.com>
2026-09-08 23:38:40 +0530

    feat: isolate Kafka configuration for tests by adding a test-specific profile and configuration class

--- src/main/java/com/care/medi/beans/KafkaConfig.java
+++ src/main/java/com/care/medi/beans/KafkaConfig.java
@@ -6,11 +6,13 @@ import org.apache.kafka.common.config.SslConfigs;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.*;
 
 import java.util.Map;
 
 @Configuration
+@Profile("!test")
 public class KafkaConfig {
 
     @Bean
